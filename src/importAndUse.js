852e47b80a8b4fe168dac0db44e2f4a606239449
Gabriel Bibiano <gabriel.bibiano.a@gmail.com>
2019-04-16 21:15:21 -0300

    Firsts steps

---
+++ src/importAndUse.js
@@ -0,0 +1,18 @@
+const latestItemFrom = array => array[array.length - 1]
+
+export default {
+  created () {
+    const { componentsToUse } = this.$options
+
+    if (componentsToUse) {
+      componentsToUse.forEach(path => {
+        let pathArray = path.split('/');
+        let componentName = latestItemFrom(pathArray)
+        componentName = componentName.replace(/.vue/gi, '')
+
+        const promise = import('@/components' + path + '.vue')
+        this.$options.components[componentName] = () => promise
+      });
+    }
+  }
+}
